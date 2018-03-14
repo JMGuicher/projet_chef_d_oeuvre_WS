64112f32a97a4ae355c0457a4dd1490edb38a944
Alexandre Mommers <alexandre.a.mommers@bnpparibas-pf.com>
2018-03-14 16:46:42 +0100

    fixe service worker generation

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -102,9 +102,9 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
             minimize: true,
             debug: false
         }),
-        new WorkboxPlugin({
+        new WorkboxPlugin.GenerateSW({
           clientsClaim: true,
-          skipWaiting: true,
+          skipWaiting: true
         }),
         new ZipPlugin({
             filename: 'angular-5-starter.zip',
